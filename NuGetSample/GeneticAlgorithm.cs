85401013925b799caa19c4fe98df952e096c3004
Svetlana <sveta.krasnova2003@yandex.ru>
2024-10-21 01:42:21 +0300

    Add the second lab

--- NuGetSample/GeneticAlgorithm.cs
+++ NuGetSample/GeneticAlgorithm.cs
@@ -1,85 +1,118 @@
-﻿namespace NuGetSample
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace NuGetSample
 {
     public class GeneticAlgorithm
     {
         private List<Solution> population = new ();
         private readonly Random random = new ();
+        private object populationLock = new object(); // Для синхронизации доступа к списку популяции
 
         // Инициализация популяции случайными решениями
-        public void InitializePopulation(int populationSize)
+        public void InitializePopulation(int populationSize, int a, int b, int c)
         {
             int index = 0;
-            while(index < populationSize)
+            Parallel.For(0, populationSize, i =>
             {
-                var squares = new List<Square>
+                while (index < populationSize)
                 {
-                    new Square(1, random.Next(0, 10), random.Next(0, 10)),
-                    new Square(1, random.Next(0, 10), random.Next(0, 10)),
-                    new Square(2, random.Next(0, 10), random.Next(0, 10)),
-                    new Square(2, random.Next(0, 10), random.Next(0, 10)),
-                    new Square(3, random.Next(0, 10), random.Next(0, 10))
-                };
+                    var squares = new List<Square>();
 
-                var solution = new Solution(squares);
-                if (solution.IsValid()) // Добавляем решение, если оно валидно
-                {
-                    population.Add(solution);
-                    ++index;
+                    for (int j = 0; j < a; j++)
+                    {
+                        squares.Add(new Square(1, random.Next(0, 10), random.Next(0, 10)));
+                    }
+                    for (int j = 0; j < b; j++)
+                    {
+                        squares.Add(new Square(2, random.Next(0, 10), random.Next(0, 10)));
+                    }
+                    for (int j = 0; j < c; j++)
+                    {
+                        squares.Add(new Square(3, random.Next(0, 10), random.Next(0, 10)));
+                    }
+
+                    var solution = new Solution(squares);
+
+
+                    // Синхронизируем доступ к общему ресурсу (список популяции)
+                    if (solution.IsValid())
+                    {
+                        lock (populationLock) // Блокировка для защиты доступа
+                        {
+                            if (index < populationSize) // Проверяем лимит популяции
+                            {
+                                population.Add(solution);
+                                index++;
+                            }
+                        }
+                    }
                 }
-            }
+            });
         }
 
         // Основной метод для запуска алгоритма
-        public Solution Run(int maxGenerations)
+        public Solution Run(int maxGenerations, CancellationToken token, Action<int, int, Solution> onProgressUpdate)
         {
             int generation = 0;
             while (generation < maxGenerations)
             {
                 generation++;
 
-                // Выбор лучшего решения в текущем поколении
-                Solution bestSolution = population.OrderBy(s => s.CalculateFitness()).First();
-
-                Console.WriteLine($"Поколение №: {generation}, Мин. площадь: {bestSolution.CalculateFitness()}");
-
-                // Следующий этап эволюции популяции
-                EvolvePopulation();
-
-                // Проверка нажатия клавиши для завершения
-                if (Console.KeyAvailable)
+                Parallel.ForEach(population, solution =>
                 {
-                    Console.ReadKey();
+                    solution.CalculateFitness();
+                });
+                
+                // Выбор лучшего решения в текущем поколении
+                Solution bestSolution = population.OrderBy(s => s.Fitnes).First();
+                onProgressUpdate(generation, bestSolution.Fitnes, bestSolution);
+                
+                if(token.IsCancellationRequested)
+                {
                     return bestSolution;
                 }
-            }
 
-            // Возвращение лучшего решения по окончании заданного числа поколений
-            return population.OrderBy(s => s.CalculateFitness()).First();
+                if (generation < maxGenerations)
+                {
+                    EvolvePopulation();
+                }
+            }
+            return population.OrderBy(s => s.Fitnes).First();
         }
 
         // Эволюция популяции
         private void EvolvePopulation()
         {
             List<Solution> newPopulation = new List<Solution>();
-
-            // Отбор лучших решений
-            var selectedSolutions = population.OrderBy(s => s.CalculateFitness()).Take(population.Count / 2).ToList();
-
-            // Кроссовер и мутация
-            while (newPopulation.Count < population.Count)
+            var selectedSolutions = population.OrderBy(s => s.Fitnes).Take(population.Count / 2).ToList();
+            Parallel.For(0, population.Count, (i) =>
             {
-                var parent1 = selectedSolutions[random.Next(selectedSolutions.Count)];
-                var parent2 = selectedSolutions[random.Next(selectedSolutions.Count)];
-
-                var child = Crossover(parent1, parent2);
-                Mutate(child);
-
-                if (child.IsValid()) // Добавляем решение, если оно валидно
+                while (newPopulation.Count < population.Count)
                 {
-                    newPopulation.Add(child);
-                }
-            }
+                    // Выбор родителей случайным образом
+                    var parent1 = selectedSolutions[random.Next(selectedSolutions.Count)];
+                    var parent2 = selectedSolutions[random.Next(selectedSolutions.Count)];
 
+                    // Выполняем кроссовер
+                    var child = Crossover(parent1, parent2);
+
+                    // Выполняем мутацию
+                    Mutate(child);
+
+                    // Проверяем валидность ребенка
+                    if (child.IsValid())
+                    {
+                        // Блокируем доступ к общему ресурсу newPopulation
+                        lock (populationLock)
+                        {
+                            if (newPopulation.Count < population.Count)
+                            // Добавляем ребенка в новую популяцию
+                            { newPopulation.Add(child); }
+                        }
+                    }
+                }
+            });
             population = newPopulation;
         }
 
--- NuGetSample/Solution.cs
+++ NuGetSample/Solution.cs
@@ -1,22 +1,25 @@
-﻿namespace NuGetSample
+﻿using System.Text;
+
+namespace NuGetSample
 {
     public class Solution
     {
         //Экземпляр расстановки квадратов
-        public List<Square> Squares { get; set; }      
+        public List<Square> Squares { get; set; }
+        public int Fitnes { get; set; }
         public Solution(List<Square> squares)
         {
             this.Squares = squares;
         }
 
         // Рассчёт площади минимального прямоугольника, содержащего все квадраты
-        public double CalculateFitness()
+        public void CalculateFitness()
         {
             int maxX = Squares.Max(s => s.X + s.SideLength);
             int minX = Squares.Min(s => s.X);
             int maxY = Squares.Max(s => s.Y + s.SideLength);
             int minY = Squares.Min(s => s.Y);
-            return (maxX - minX) * (maxY - minY); // Площадь прямоугольника
+            Fitnes = (maxX - minX) * (maxY - minY); // Площадь прямоугольника
         }
 
         // Проверка на пересечение квадратов
@@ -40,6 +43,21 @@
         {
             return !(a.X + a.SideLength <= b.X || b.X + b.SideLength <= a.X ||
                      a.Y + a.SideLength <= b.Y || b.Y + b.SideLength <= a.Y);
+        }
+        public override string ToString()
+        {
+            int k = 0;
+            var res = new StringBuilder();
+            foreach (var s in Squares) 
+            {
+                k += 1;
+                res.Append($"{s.SideLength}: ({s.X}, {s.Y}); ");
+                if (k == 5 || k == 10)
+                {
+                    res.Append("\n");
+                }
+            }
+            return res.ToString();
         }
     }
 
